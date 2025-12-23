278dc2675290798ca07833e4b861232e9553d985
Gabriel Padilla <g.padilla@uniandes.edu.co>
2025-12-22 22:54:44 -0500

    api store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "antd/dist/antd.css";
+import store from "./app/store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
