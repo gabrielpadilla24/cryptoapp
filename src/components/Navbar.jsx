9175fd021cde94b1f7f00c0adbb75b6fa1f2e1d6
Gabriel Padilla <g.padilla@uniandes.edu.co>
2025-12-22 22:13:19 -0500

    works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import {Button, Menu, Typography, Avatar} from 'antd'
-import {Link} from 'react-router-dom'
-import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
-import icon from '../images/cryptocurrency.png'
+import React from "react";
+import { Avatar, Typography } from "antd";
+import { Link } from "react-router-dom";
+import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
-        <div className="logo-container">
-            <Avatar src={icon} size="large" />
-            <Typography.Title level={2} className="logo">
-                <Link to="/">CryptoApp</Link>
-            </Typography.Title>
-            {/* <Button className="menu-control-container"></Button> */}
-        </div>
-
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">CryptoApp</Link>
+        </Typography.Title>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
